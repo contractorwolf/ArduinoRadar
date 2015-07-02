8285cb13c554bed60ea0063161de24d43d7b9ebb
james wolf <contractorwolf@hotmail.com>
2015-07-01 23:11:03 -0400

    initial checkin

---
+++ ArduinoRadar/ArduinoRadarForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ArduinoRadar
+{
+    partial class ArduinoRadarForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtIncoming = new System.Windows.Forms.TextBox();
+            this.cbPorts = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(503, -1);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtIncoming
+            // 
+            this.txtIncoming.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIncoming.Location = new System.Drawing.Point(12, 2);
+            this.txtIncoming.Name = "txtIncoming";
+            this.txtIncoming.Size = new System.Drawing.Size(333, 38);
+            this.txtIncoming.TabIndex = 1;
+            // 
+            // cbPorts
+            // 
+            this.cbPorts.FormattingEnabled = true;
+            this.cbPorts.Location = new System.Drawing.Point(376, 1);
+            this.cbPorts.Name = "cbPorts";
+            this.cbPorts.Size = new System.Drawing.Size(121, 21);
+            this.cbPorts.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(12, 46);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(566, 319);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // ArduinoRadarForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 377);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.cbPorts);
+            this.Controls.Add(this.txtIncoming);
+            this.Controls.Add(this.btnStart);
+            this.Name = "ArduinoRadarForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtIncoming;
+        private System.Windows.Forms.ComboBox cbPorts;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ ArduinoRadar/ArduinoRadarForm.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace ArduinoRadar
+{
+    public partial class ArduinoRadarForm : Form
+    {
+
+        private List<RadarDot> dots = new List<RadarDot>();
+        public static SerialPort arduino = null;
+
+        public static byte[] rx_data;
+
+
+        public static int bytesToRead = 0;
+        public static int read_count = 0;
+        public static int count = 0;
+        public static double freq = 0;
+
+        private int totalCount = 0;
+        private int missCount = 0;
+        private static int totalMissedByteLength = 0;
+
+
+        private string lastMessage = "";
+        private string message = "";
+
+
+
+        private byte _terminator = 0x4; 
+
+        public ArduinoRadarForm()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            arduino = new SerialPort(cbPorts.SelectedValue.ToString(), 9600);
+            arduino.Open();
+            arduino.DataReceived += new SerialDataReceivedEventHandler(Arduino_DataReceived);
+        }
+
+
+        private void Arduino_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+
+            try
+            {
+                rx_data = new byte[arduino.BytesToRead];
+
+                // read the data
+                read_count = arduino.Read(rx_data, 0, rx_data.Length);
+
+
+                lastMessage += Encoding.ASCII.GetString(rx_data, 0, read_count);
+
+
+
+
+                if (lastMessage.IndexOf("*") > -1) 
+                {
+
+
+                    message = lastMessage;
+                    lastMessage = "";
+                    this.Invoke(new EventHandler(WriteData));
+
+
+                } 
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbPorts.DataSource = SerialPort.GetPortNames();
+            cbPorts.Update();
+        }
+
+        private void WriteData(object sender, EventArgs e)
+        {
+
+            txtIncoming.Text = ParseMessage(message);
+
+
+
+        }
+
+
+
+        private string ParseMessage(string message)
+        {
+            if (message.IndexOf("*") != -1)
+            {//then * exists
+
+                message = message.Substring(0, message.IndexOf("*"));
+
+                if (message.IndexOf(",") != -1)
+                {//then , exists
+
+
+                    string[] pair = message.Split(',');
+
+                    if (pair.Length == 2)
+                    {
+                        string strangle = pair[0];
+                        string strdistance = pair[1];
+
+
+
+                        int distance = 0;
+                        int angle = 0;
+
+                        int.TryParse(strangle, out angle);
+                        int.TryParse(strdistance, out distance);
+
+
+
+                        //covert servo to 360 
+                        angle = (int)(angle / 4.444);
+                        //and extend distance for visualization
+                        distance = distance * 5;
+
+                        //and clear the collection on every other pass
+                        if (angle == 0)
+                        {
+                            dots.Clear();
+                        }
+
+
+
+                        dots.Add(new RadarDot(angle, distance));
+
+
+                        panel1.Refresh();
+
+
+
+
+                    }
+                    else
+                    {//stub?
+                        message = "error";
+                    }
+
+
+
+
+                }
+                else
+                {//stub?
+                    message = "error";
+                }
+
+            }
+            else
+            {//stub?
+                    message = "error";
+
+            }
+
+            return (message);
+
+
+        }
+
+
+        public class RadarDot
+        {
+            public int Distance { get; set; }
+            public int Angle { get; set; }
+
+
+            public RadarDot(int angle, int distance)
+            {
+                Angle = angle;
+                Distance = distance;
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawRadar(e.Graphics);
+        }
+
+
+        private void DrawRadar(Graphics g)
+        {
+            int offset = 5;
+
+
+            int width = panel1.Width - offset;// 50;
+            int height = panel1.Height - offset;//50;
+            int radius = (width - offset) / 2;
+
+
+            g.FillEllipse(new SolidBrush(System.Drawing.Color.White), 0, 0, width, height);
+
+            Point center = new Point((width / 2) + (offset / 2), (height / 2) + (offset / 2));
+
+
+            //center dot
+            g.FillRectangle(new SolidBrush(System.Drawing.Color.Black), center.X, center.Y, 5, 5);
+
+
+            DrawDotCollection(g, center);
+
+        }
+
+
+        private void DrawDotCollection(Graphics g, Point center)
+        {
+            SolidBrush greenBrush = new SolidBrush(System.Drawing.Color.Green);
+            foreach (RadarDot dot in dots)
+            {
+
+                Point endpoint = FindEndpointOnCircle(center, dot.Distance, dot.Angle);
+
+                g.FillRectangle(greenBrush, endpoint.X, endpoint.Y, 10, 10);
+
+            }
+        }
+
+
+
+
+        private Point FindEndpointOnCircle(Point origin, int radius, int angle)
+        {
+            double x = origin.X + (radius * Math.Cos(angle * Math.PI / 180F));
+            double y = origin.Y - (radius * Math.Sin(angle * Math.PI / 180F));
+
+            return new Point((int)x, (int)y);
+        }
+    }
+}
